1554055d8afde94e32757f1ead381c4fc0275596
Chain Yu <yujnln@gmail.com>
2014-08-05 14:08:13 +0800

    parse bin file.

--- HexBinWorker/HexBinWorker/HexBinWorker.cpp
+++ HexBinWorker/HexBinWorker/HexBinWorker.cpp
@@ -87,7 +87,7 @@ BOOL CHexBinWorkerApp::InitInstance()
 		delete pShellManager;
 	}
 
-	// 由于对话框已关闭，所以将返回 FALSE 以便退出应用程序，
+	//  由于对话框已关闭，所以将返回 FALSE 以便退出应用程序，
 	//  而不是启动应用程序的消息泵。
 	return FALSE;
 }
